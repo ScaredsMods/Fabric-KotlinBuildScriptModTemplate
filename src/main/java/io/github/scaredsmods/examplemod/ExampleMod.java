8894b8602a7b1311ae432fd646837693268ca0e1
ScaredRabbit <scaredrabbitnl@gmail.com>
2025-11-01 14:33:01 +0100

    random changes

---
+++ src/main/java/io/github/scaredsmods/examplemod/ExampleMod.java
@@ -0,0 +1,11 @@
+package io.github.scaredsmods.examplemod;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ExampleMod implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
